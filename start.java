339cc9b4220a9e06cf3a63720dd1234463616e62
ReDThunDeR33 <mdarko190@gmail.com>
2024-05-10 20:24:57 +0600

    new

---
+++ start.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+import javax.swing.*;
+
+
+public class start{
+public static void main (String[] args){
+	
+	RegisterFrame R1 = new RegisterFrame();
+	R1.setVisible(true);
+	
+	
+}
+}
